a18330f9321b6bca458a75089014b1630081d399
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2024-01-28 02:26:43 +0300

    1.Layout duzenlendi urunler controller oluşturuldu ve urunlerındex de oluşturuldu. düzeni verildi 2. Kategoriler listelendi

---
+++ MobilyaETicaret.Web/Models/UrunlerViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MobilyaETicaret.Core.MobilyaETicaretDatabase;
+
+namespace MobilyaETicaret.Web.Models
+{
+	public class UrunlerViewModel
+	{
+		public IEnumerable<Urunler> Urunler { get; set; }
+		public IEnumerable<Kategoriler> Kategoriler { get; set; }
+	}
+}
