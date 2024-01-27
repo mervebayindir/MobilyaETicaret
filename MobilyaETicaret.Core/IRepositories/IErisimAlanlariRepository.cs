31617bfc8efe42dbed7f29605f60e3bb748592b2
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2024-01-27 12:23:33 +0300

    Erisım alanı repository ve service oluşturuldu

---
+++ MobilyaETicaret.Core/IRepositories/IErisimAlanlariRepository.cs
@@ -0,0 +1,14 @@
+﻿using MobilyaETicaret.Core.MobilyaETicaretDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilyaETicaret.Core.IRepositories
+{
+    public interface IErisimAlanlariRepository : IGenericRepository<ErisimAlanlari>
+    {
+        Task<ErisimAlanlari> ErisimAlaniSil(int id);
+    }
+}
