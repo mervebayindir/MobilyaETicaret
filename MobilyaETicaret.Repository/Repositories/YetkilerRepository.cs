7fa93199344a8a474d908bdd85e76276221c0505
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2024-01-30 21:49:29 +0300

    1.Yetkiler repository ve servisleri oluşturuldu

---
+++ MobilyaETicaret.Repository/Repositories/YetkilerRepository.cs
@@ -0,0 +1,16 @@
+﻿using MobilyaETicaret.Core.MobilyaETicaretDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilyaETicaret.Repository.Repositories
+{
+    public class YetkilerRepository : GenericRepository<Yetkiler>
+    {
+        public YetkilerRepository(AppDbContext mobilyaETicaretDB) : base(mobilyaETicaretDB)
+        {
+        }
+    }
+}
