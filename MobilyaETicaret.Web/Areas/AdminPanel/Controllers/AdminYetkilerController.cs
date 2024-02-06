e18db316e49c25aee64e36ea812fc1fa5f6a1454
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2024-02-07 00:19:42 +0300

    Yetki ekleme başarılı

--- MobilyaETicaret.Web/Areas/AdminPanel/Controllers/AdminYetkilerController.cs
+++ MobilyaETicaret.Web/Areas/AdminPanel/Controllers/AdminYetkilerController.cs
@@ -8,11 +8,13 @@ namespace MobilyaETicaret.Web.Areas.AdminPanel.Controllers
     {
         private readonly IYetkilerService _yetkilerService;
         private readonly IErisimAlanlariService _erisimAlanlariService;
+        private readonly IYetkiErisimService _yetkiErisimService;
 
-        public AdminYetkilerController(IYetkilerService yetkilerService, IErisimAlanlariService erisimAlanlariService)
+        public AdminYetkilerController(IYetkilerService yetkilerService, IErisimAlanlariService erisimAlanlariService, IYetkiErisimService yetkiErisimService)
         {
             _yetkilerService = yetkilerService;
             _erisimAlanlariService = erisimAlanlariService;
+            _yetkiErisimService = yetkiErisimService;
         }
 
         public async Task<IActionResult> AdminYetkilerIndex()
@@ -32,9 +34,26 @@ namespace MobilyaETicaret.Web.Areas.AdminPanel.Controllers
         [HttpPost]
         public async Task<IActionResult> AdminYetkiKaydetIndex(Yetkiler yetkiler, int[] secilenSayfalar)
         {
-            var yetkilerList = await _yetkilerService.GetAllAsyncs();
-            ViewBag.erisimAlani = await _erisimAlanlariService.GetAllAsyncs();
-            return View(yetkiler);
+            YetkiErisim yeniYetkiErisim = new YetkiErisim();
+            if (ModelState.IsValid)
+            {
+                yetkiler.AktifMi = true;
+                yetkiler.EklenmeTarih = DateTime.Now;
+                var sonuc = await _yetkilerService.AddAsync(yetkiler);
+                if (sonuc != null)
+                {
+                    foreach (var item in secilenSayfalar)
+                    {
+                        yeniYetkiErisim.YetkiId = yetkiler.Id;
+                        yeniYetkiErisim.ErisimAlaniId = item;
+                        yeniYetkiErisim.Aciklama = yetkiler.YetkiAdi;
+                        yeniYetkiErisim.EklenmeTarihi = DateTime.Now;
+                        await _yetkiErisimService.AddAsync(yeniYetkiErisim);
+                    }
+                    return RedirectToAction("AdminYetkilerIndex");
+                }
+            }
+            return View();
         }
     }
 }
